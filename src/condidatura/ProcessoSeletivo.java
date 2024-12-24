4a4442b069e693ddce8b23d4bd74fec936f8be80
Ruan Torquato <ruan.tor4@hotmail.com>
2024-12-24 11:30:14 -0300

    criando projeto

---
+++ src/condidatura/ProcessoSeletivo.java
@@ -0,0 +1,15 @@
+package condidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo Seletivo");
+    }
+        static void analisarCandidato(){
+            double slarioBase = 2000.0;
+            if (salarioBase< salarioPretendido) {
+                
+            }
+        }
+
+   
+}
